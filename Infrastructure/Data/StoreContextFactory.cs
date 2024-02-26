f0ff2b0ba534853f69f18bb416412a1462055edd
dvvin <131934199+dvvin@users.noreply.github.com>
2024-02-25 23:09:02 -0600

    Initial Commit.

---
+++ Infrastructure/Data/StoreContextFactory.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextFactory : IDesignTimeDbContextFactory<StoreContext>
+    {
+        public StoreContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Development.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<StoreContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlite(connectionString);
+
+            return new StoreContext(optionsBuilder.Options);
+        }
+    }
+}
