cc34965f40be3dec717946292ae2766356e1618b
dvvin <131934199+dvvin@users.noreply.github.com>
2024-03-25 23:32:30 -0500

    Home page updated.

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  logoImageUrl = '../assets/images/logo-color.png';
+  bootImageUrl = '../assets/images/boot-ang1.png';
+  boardImageUrl = '../assets/images/sb-ang1.png';
+  gloveImageUrl = '../assets/images/glove-code1.png';
+  hatImageUrl = '../assets/images/hat-core1.png';
 }
